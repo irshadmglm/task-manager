c1bdf164fb5c33bf6e49b5abac730be7997aa768
irshad <irshadmglm@gmail.com>
2025-02-20 21:37:42 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/task-manager/frontend/', // Adjust based on your repository structure
+  base: '/task-manager/',
 });
