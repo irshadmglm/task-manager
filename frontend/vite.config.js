9db0b100fa9f984159a81c1d11ae21cf6bf0f477
irshad <irshadmglm@gmail.com>
2025-02-20 17:26:10 +0530

    ready to deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace 'your-username' and 'your-repo-name'
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/task-manager/frontend/',  // Must match the repo's subdirectory
+});
