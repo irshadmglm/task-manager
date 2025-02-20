1068a74911d35344ca1aef1fc6bef548dd314d17
irshad <irshadmglm@gmail.com>
2025-02-20 19:44:27 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Replace 'your-username' and 'your-repo-name'
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/task-manager/frontend/',  // Must match the repo's subdirectory
-});
+})
